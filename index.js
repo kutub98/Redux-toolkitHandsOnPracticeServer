e87b50351896dbda56ee654ea12e31d9be51fe82
Kutub Udd <socialnetwork98710@gmail.com>
2024-04-17 19:14:30 +0600

    added

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { ObjectId, MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 require('dotenv').config();
 app.use(cors());
-
+app.use(express.json());
 const uri = process.env.DB_URL;
 const client = new MongoClient(uri, {
   serverApi: {
@@ -30,11 +30,29 @@ async function run() {
 // database name
 const database = client.db('ReduxRtkQuery');
 const collection = database.collection('Tasklist');
+
+app.delete('/task/:id', async (req, res) => {
+  const id = req.params.id;
+  console.log(id);
+  const result = await collection.deleteOne({ _id: new ObjectId(id) });
+  console.log(result);
+});
+
 app.get('/task', async (req, res) => {
   try {
     const tasklist = await collection.find({}).toArray();
-    console.log(tasklist, 'helloTasklist');
     res.json(tasklist);
+  } catch (error) {
+    console.error('Something went wrong:', error);
+    res.status(500).json({ error: error.message });
+  }
+});
+app.post('/task', async (req, res) => {
+  try {
+    const taskDetails = req.body;
+    const newTask = await collection.insertOne(taskDetails);
+
+    res.send(newTask);
   } catch (error) {
     console.error('Something went wrong:', error);
     res.status(500).json({ error: error.message });
