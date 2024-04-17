95af4d50abfc7cf9ef8240ce7a9c9a3f5d66c2a2
Kutub Udd <socialnetwork98710@gmail.com>
2024-04-17 08:55:50 +0600

    added

--- index.js
+++ index.js
@@ -1,6 +1,35 @@
 const express = require('express');
 const cors = require('cors');
 
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const app = express();
+const port = process.env.PORT || 5000;
+require('dotenv').config();
+app.use(cors());
+
+// const uri = process.env.DB_URL;
+const uri = `mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASS}@cluster0.mlxcjcs.mongodb.net/ReduxRtkQuery?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    await client.connect();
+
+    console.log(
+      'Pinged your deployment. You successfully connected to MongoDB!',
+    );
+  } catch (error) {
+    console.log(error);
+  }
+}
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
